32e49513d8fd37dae88a3bfc7953b4419327bdf8
veha2309 <157272105+veha2309@users.noreply.github.com>
2024-12-06 22:21:10 +0530

    fix

--- app/api/profile/[userId]/route.ts
+++ app/api/profile/[userId]/route.ts
@@ -1,40 +1,42 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prismadb";
 
+interface Context {
+  params: {
+    userId: string;
+  };
+}
 
+export async function GET(request: Request, context: Context) {
+  try {
+    const { userId } = context.params;
 
-export async function GET(
-    request: Request,
-    { params }: { params: { userId: string } }
-) {
-    try{const { userId } = params;
-
-        if (!userId) {
-            return NextResponse.json(
-                { error: "User ID is missing" },
-                { status: 400 }
-            );
-        }
-
-        // Fetch the user from the database
-        const user = await prisma.user.findUnique({
-            where: { id: userId },
-        });
-
-        if (!user) {
-            return NextResponse.json(
-                { error: "User not found" },
-                { status: 404 }
-            );
-        }
-
-        // Send user data as JSON
-        return NextResponse.json(user);
-    } catch (error) {
-        console.error("Error fetching user profile:", error);
-        return NextResponse.json(
-            { error: "Internal Server Error" },
-            { status: 500 }
-        );
+    if (!userId) {
+      return NextResponse.json(
+        { error: "User ID is missing" },
+        { status: 400 }
+      );
     }
+
+    // Fetch the user from the database
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        { error: "User not found" },
+        { status: 404 }
+      );
+    }
+
+    // Send user data as JSON
+    return NextResponse.json(user);
+  } catch (error) {
+    console.error("Error fetching user profile:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
